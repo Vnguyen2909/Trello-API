28e405f9638e0c81f2a51b9287d4ccf272bd3d04
Vnguyen2909 <nguyenvi290903@gmail.com>
2026-05-04 17:49:51 +0700

    Init code Base

--- src/server.js
+++ src/server.js
@@ -1,15 +1,27 @@
-import express from "express"
+import express from 'express'
+import { mapOrder } from '~/utils/sorts.js'
 
 const app = express()
 
 const hostname = 'localhost'
+const port = 8017
 
-const port = 8025
-
-app.get('/', function(req, res) {
-    res.send('Hello Nguyenv2909')
+app.get('/', (req, res) => {
+  // Test Absolute import mapOrder
+  // eslint-disable-next-line no-console
+  console.log(mapOrder(
+    [{ id: 'id-1', name: 'One' },
+      { id: 'id-2', name: 'Two' },
+      { id: 'id-3', name: 'Three' },
+      { id: 'id-4', name: 'Four' },
+      { id: 'id-5', name: 'Five' }],
+    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
+    'id'
+  ))
+  res.end('<h1>Hello World!</h1><hr>')
 })
 
-app.listen(port, hostname, ()=> {
-    console.log(`Hello Nguyenv2909, I'm running sever at http://${hostname}:${port}/`)
+app.listen(port, hostname, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Ngv2909 - I am running at http://${ hostname }:${ port }/`)
 })
