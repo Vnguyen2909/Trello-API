1cd13faa3eab148254b2bac88517866adec7402b
Vnguyen2909 <nguyenvi290903@gmail.com>
2026-05-07 20:08:01 +0700

    Validation with library Joi

--- src/server.js
+++ src/server.js
@@ -8,6 +8,10 @@ import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
 const START_SERVER = () => {
   const app = express()
 
+  //Enable req.body json data
+  app.use(express.json())
+
+  //Use APIs v1
   app.use('/v1', APIs_v1)
 
   app.listen(env.APP_PORT, env.APP_HOST, () => {
