c326fd6ef375c1affcfe0356f5eb9f60ed16e589
Vnguyen2909 <nguyenvi290903@gmail.com>
2026-05-08 20:20:00 +0700

    Error Handling

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,18 +1,12 @@
 import { StatusCodes } from 'http-status-codes'
 
-const createNew = async (req, res, next) => { 
+const createNew = async (req, res, next) => {
   try {
-    console.log(req.body)
-
     //Dieu huong du lieu sang tang Service
-
+    
     //Co ket qua thi tra ve phia Client
     res.status(StatusCodes.CREATED).json({ message: 'POST from Controller: API create new board' })
-  } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      errors: error.message
-    })
-  }
+  } catch (error) {next(error)}
 }
 
 export const boardController = {
--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -2,6 +2,7 @@
 
 import Joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
+import ApiError from '~/utils/ApiError'
 
 const createNew = async (req, res, next) => {
   const conrrectCondition = Joi.object({
@@ -15,10 +16,9 @@ const createNew = async (req, res, next) => {
     //Validation du lieu hop le thi cho request di tiep sang Controller
     next()
   } catch (error) {
-    console.log(error),
-    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
-      errors: new Error(error).message
-    })
+    const errorMessage = new Error(error).message
+    const customError = new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, errorMessage)
+    next(customError)
   }
 }
 
