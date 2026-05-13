6a6072e24000e1b29921b329fb1b8106a03a3dbd
Vnguyen2909 <nguyenvi290903@gmail.com>
2026-05-13 15:02:45 +0700

    Create Api Get Details Of Board

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -2,6 +2,8 @@
 
 import { slugify } from '~/utils/formatter'
 import { boardModel } from '~/models/boardModel'
+import ApiError from '~/utils/ApiError'
+import { StatusCodes } from 'http-status-codes'
 
 const createNew = async (reqbody) => {
   try {
@@ -22,6 +24,17 @@ const createNew = async (reqbody) => {
   } catch (error) { throw error }
 }
 
+const getDetails = async (boardId) => {
+  try {
+    const board = await boardModel.getDetails(boardId)
+    if (!board) {
+      throw new ApiError(StatusCodes.NOT_FOUND, 'Board not found!')
+    }
+
+    return board
+  } catch (error) { throw error }
+}
+
 export const boardService = {
-  createNew
+  createNew, getDetails
 }
